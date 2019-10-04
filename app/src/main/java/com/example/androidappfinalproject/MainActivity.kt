3500b08e8a6812cb9da0780042425a4336ff8f20
FusProgramming <34615176+FusProgramming@users.noreply.github.com>
2019-10-04 02:18:24 -0400

    Database Writes working when registering

--- app/src/main/java/com/example/androidappfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/androidappfinalproject/MainActivity.kt
@@ -3,19 +3,31 @@ package com.example.androidappfinalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_nav_bar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavViewBar.onNavigationItemSelectedListener = mOnNavigationItemSelectedListener
 
-        buttonToLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
     }
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_notifications -> {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    return@OnNavigationItemSelectedListener true
 
-
+                }
+                else -> false
+            }
+        }
 }
+
+
+
