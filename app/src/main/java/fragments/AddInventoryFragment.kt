6b26f5be8dbef97c748eede61f8a076c536cbd51
Robert Fus <34615176+FusProgramming@users.noreply.github.com>
2019-10-24 08:32:37 -0400

    Admin Activity Profile

---
+++ app/src/main/java/fragments/AddInventoryFragment.kt
@@ -0,0 +1,21 @@
+package fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.androidappfinalproject.LoginActivity
+import com.example.androidappfinalproject.R
+
+class AddInventoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_inventory, container, false)
+    }
+    
+}
---
+++ app/src/main/java/fragments/SearchFragment.kt
@@ -0,0 +1,16 @@
+package fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.androidappfinalproject.R
+
+class SearchFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_user_search_bar, container, false)
+    }
+}
