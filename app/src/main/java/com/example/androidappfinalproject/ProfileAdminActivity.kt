a9fd326a584b40485ce06abb45db52fc7cdcb438
Robert Fus <34615176+FusProgramming@users.noreply.github.com>
2019-10-24 03:36:01 -0400

    Admin Activity Profile

--- app/src/main/java/com/example/androidappfinalproject/ProfileAdminActivity.kt
+++ app/src/main/java/com/example/androidappfinalproject/ProfileAdminActivity.kt
@@ -2,11 +2,40 @@ package com.example.androidappfinalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fragments.ProfileFragment
 
 class ProfileAdminActivity : AppCompatActivity() {
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item->
+        when(item.itemId){
+            R.id.home -> {
+                replaceFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.map -> {
+                replaceFragment(MapFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.cart -> {
+                replaceFragment(CartFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
 
+        false
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_admin)
+        setContentView(R.layout.activity_admin_profile)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
+
